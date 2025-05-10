6403ec0668d21d57abaef489cdb0ccb74177080c
Traman2 <tejassraman@gmail.com>
2025-05-10 03:12:42 -0500

    test

--- express_services/server.js
+++ express_services/server.js
@@ -11,5 +11,5 @@ app.get('/main', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on PORT ${PORT}`);
 })
